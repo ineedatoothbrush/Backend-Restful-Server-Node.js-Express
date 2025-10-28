32ef7f6b1f9b3396e4bb23dacf1211be56168f70
ineedatoothbrush <daod1278@gmail.com>
2025-10-28 17:56:10 +0700

    .

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const port = 8080
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+app.get('/daominhduc', (req, res) => {
+    res.send('Hello Dao Minh Duc!')
+})
+
+app.get('/DucDao', (req, res) => {
+    res.send('<h1>Duc Dao</h1>')
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
