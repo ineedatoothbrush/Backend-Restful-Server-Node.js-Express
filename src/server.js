c3826571370bae3606e5817cf6b94a9b67de6481
ineedatoothbrush <daod1278@gmail.com>
2025-10-28 18:13:57 +0700

    .

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 8080
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 app.get('/daominhduc', (req, res) => {
-    res.send('Hello Dao Minh Duc!')
+    // res.send('Hello Dao Minh Duc!')
+    res.render('sample.ejs')
 })
 
 app.get('/DucDao', (req, res) => {
