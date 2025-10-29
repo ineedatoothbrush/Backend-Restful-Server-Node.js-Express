77a165c0f7e450acda472076c7f5b5dceace851c
ineedatoothbrush <daod1278@gmail.com>
2025-10-29 17:05:21 +0700

    Static files

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const path = require('path');
 require('dotenv').config();
+
+app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 
