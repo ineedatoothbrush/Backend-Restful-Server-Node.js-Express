7d08164c75aee8379fac71dafa22b813c00cbd05
ineedatoothbrush <daod1278@gmail.com>
2025-10-31 13:24:48 +0700

    .

--- src/server.js
+++ src/server.js
@@ -5,19 +5,13 @@ const port = process.env.PORT || 8081
 const hostname = process.env.HOST_NAME
 const configViewEngine = require('./config/viewEngine');
 const webRoute = require('./routes/web');
-const mysql = require('mysql2')
+const connection = require('./config/database');
 
 configViewEngine(app);
 app.use('/', webRoute);
 
 //test connection
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'hoidanit',
-    port: 3307,
-    password: '123456'
-});
+
 connection.query(
     'SELECT * FROM Users u',
     function (err, results, fields) {
@@ -25,6 +19,7 @@ connection.query(
         console.log(">>>>>>field = ", fields); // fields contains extra meta data about results, if available
     }
 );
+
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
 })
